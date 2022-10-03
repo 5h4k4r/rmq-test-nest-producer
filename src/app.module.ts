2f29ea814eba2f3572656f856fc2671559df9ad0
5h4k4r <shakar.halabjai@yahoo.com>
2022-10-03 21:36:54 +0300

    produce a message to rabbitmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'GREETING_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://production-rabbitmqcluster:5672'],
+          queue: 'cats_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('GREETING_SERVICE') private client: ClientProxy
+  ) { }
+
+  async getHello() {
+    const message = await firstValueFrom(this.client.send<string, string>('greeting-async', 'Progressive Coder'));
+    return message;
   }
 }
